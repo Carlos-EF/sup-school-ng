f49186cfbe5fa6ca317c3a0aa5a749b5c02f7fd8
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-09-21 17:17:16 -0300

    Feito exercício 01 (commit de teste).

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CalculadoraComponent } from './components/calculadora/calculadora.component';
 import { ListaPessoasComponent } from './components/lista-pessoas/lista-pessoas.component';
+import { CalculadoraRetanguloComponent } from './components/calculadora-retangulo/calculadora-retangulo.component';
 
 export const routes: Routes = [
     { path: "calculadora", component: CalculadoraComponent },
-    { path: "lista-pessoas", component: ListaPessoasComponent }
+    { path: "lista-pessoas", component: ListaPessoasComponent },
+    { path: "calculadora-retangulo", component: CalculadoraRetanguloComponent}
 ];
 // SPA: Single Page Application
