c35c5ccd7033f8f3134b3e561630d86e8895b2f9
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-09-21 18:35:45 -0300

    Feito exercício 02.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CalculadoraComponent } from './components/calculadora/calculadora.component';
 import { ListaPessoasComponent } from './components/lista-pessoas/lista-pessoas.component';
 import { CalculadoraRetanguloComponent } from './components/calculadora-retangulo/calculadora-retangulo.component';
+import { CalculadoraMediaComponent } from './components/calculadora-media/calculadora-media.component';
 
 export const routes: Routes = [
     { path: "calculadora", component: CalculadoraComponent },
     { path: "lista-pessoas", component: ListaPessoasComponent },
-    { path: "calculadora-retangulo", component: CalculadoraRetanguloComponent}
+    { path: "calculadora-retangulo", component: CalculadoraRetanguloComponent},
+    { path: "calculadora-media", component: CalculadoraMediaComponent}
 ];
 // SPA: Single Page Application
