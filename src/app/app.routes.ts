e1a854050fe92f952aa4bc0ffa2dc700ed4756d7
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-09-22 10:12:38 -0300

    Finalizado lista de exercícios.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { CalculadoraComponent } from './components/calculadora/calculadora.compo
 import { ListaPessoasComponent } from './components/lista-pessoas/lista-pessoas.component';
 import { CalculadoraRetanguloComponent } from './components/calculadora-retangulo/calculadora-retangulo.component';
 import { CalculadoraMediaComponent } from './components/calculadora-media/calculadora-media.component';
+import { CalculadoraTemperaturaComponent } from './components/calculadora-temperatura/calculadora-temperatura.component';
+import { ListaTarefasComponent } from './components/lista-tarefas/lista-tarefas.component';
 
 export const routes: Routes = [
     { path: "calculadora", component: CalculadoraComponent },
     { path: "lista-pessoas", component: ListaPessoasComponent },
     { path: "calculadora-retangulo", component: CalculadoraRetanguloComponent},
-    { path: "calculadora-media", component: CalculadoraMediaComponent}
+    { path: "calculadora-media", component: CalculadoraMediaComponent},
+    { path: "calculadora-temperatura", component: CalculadoraTemperaturaComponent},
+    { path: "lista-tarefas", component: ListaTarefasComponent}
 ];
 // SPA: Single Page Application
