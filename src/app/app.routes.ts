fa7876421b9bf9d8908b3277fe11117600fcfb0b
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-09-22 21:55:15 -0300

    Aula 2025-09-22.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,17 @@ import { CalculadoraRetanguloComponent } from './components/calculadora-retangul
 import { CalculadoraMediaComponent } from './components/calculadora-media/calculadora-media.component';
 import { CalculadoraTemperaturaComponent } from './components/calculadora-temperatura/calculadora-temperatura.component';
 import { ListaTarefasComponent } from './components/lista-tarefas/lista-tarefas.component';
+import { ListaAlunosComponent } from './components/lista-alunos/lista-alunos.component';
+import { CadastroAlunoComponent } from './components/cadastro-aluno/cadastro-aluno.component';
 
 export const routes: Routes = [
     { path: "calculadora", component: CalculadoraComponent },
     { path: "lista-pessoas", component: ListaPessoasComponent },
-    { path: "calculadora-retangulo", component: CalculadoraRetanguloComponent},
-    { path: "calculadora-media", component: CalculadoraMediaComponent},
-    { path: "calculadora-temperatura", component: CalculadoraTemperaturaComponent},
-    { path: "lista-tarefas", component: ListaTarefasComponent}
+    { path: "calculadora-retangulo", component: CalculadoraRetanguloComponent },
+    { path: "calculadora-media", component: CalculadoraMediaComponent },
+    { path: "calculadora-temperatura", component: CalculadoraTemperaturaComponent },
+    { path: "lista-tarefas", component: ListaTarefasComponent },
+    { path: "alunos", component: ListaAlunosComponent },
+    { path: "alunos/cadastro", component: CadastroAlunoComponent}
 ];
 // SPA: Single Page Application
