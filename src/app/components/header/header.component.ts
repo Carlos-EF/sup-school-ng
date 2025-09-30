ab96b85929f8251effe2e94cc8645fbd8b7e1dc4
Carlos Eduardo Felix Junior <110876@PROWAY.TREINA>
2025-09-29 21:57:16 -0300

    aula data 2025-09-29.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavbarComponent, NavbarMenu } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+menus: NavbarMenu[];
 
+  constructor() {
+    this.menus = [
+      { link: "calculadora", titulo: "Calculadora" },
+      { link: "lista-pessoas", titulo: "Lista de Pessoas" },
+      { link: "calculadora-retangulo", titulo: "Calcular Retângulo" },
+      { link: "calculadora-media", titulo: "Calcular Média" },
+      { link: "calculadora-temperatura", titulo: "Calcular Temperatura" },
+      { link: "lista-tarefas", titulo: "Lista de Tarefas" },
+      { link: "alunos", titulo: "Alunos" },
+      { link: "turmas", titulo: "Turmas" },
+      { link: "materias", titulo: "Matérias" },
+      { link: "professores", titulo: "Professores" }
+    ]
+  }
 }
